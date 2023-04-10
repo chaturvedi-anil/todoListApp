b05de7ddeae54d740b20f3a3dd07412c516c32c5
chaturvedi-anil <chaturvedia560@gmail.com>
2023-04-10 13:27:35 +0530

    all feture added except ( line thorugh is not setup properly)

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -2,15 +2,20 @@ function giveIdToDeleteBtn(id, checkBox)
 {
     let descriptionPara = document.getElementById('description-para');
     let datePara = document.getElementById('date-para');
+
+    
     if(checkBox.checked)
     {
         descriptionPara.style.textDecoration="line-through";
         datePara.style.textDecoration="line-through";
-        const deleteButton=document.getElementById('delete-anchor').href=`/delete-task/?id=${id}`;
+        document.getElementById('delete-anchor').href=`/delete-task/?id=${id}`;
+        return;
     }
     else
     {
         descriptionPara.style.textDecoration="none";
         datePara.style.textDecoration="none";
+        document.getElementById('delete-anchor').href=`/delete-task`;
+        return;
     }
 }            
